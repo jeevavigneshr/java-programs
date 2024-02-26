7c6fbebf2de260365fdc06d7ccd82a803a946ed1
jeevavigneshr <143730328+jeevavigneshr@users.noreply.github.com>
2024-02-26 20:09:21 +0530

    Add files via upload

---
+++ service.java
@@ -0,0 +1,23 @@
+package game;
+
+public class service {
+
+	public static void main(String[] args) {
+ int a =20;
+ int b =15;
+ int c =90;
+ if(a > b && a > c) {
+  System.out.println("a is largest :"+a);
+ }else if(b > a && b > c) {
+	 System.out.println("b is largest :"+b);
+ }else {
+	 System.out.println("c is greatest :" +c);
+		 
+		  System.out.println();
+	}
+
+
+	}
+
+
+}
