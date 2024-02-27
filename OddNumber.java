1cad0ef3316d35388ff461fde7dd6a9e6e169a15
jeevavigneshr <143730328+jeevavigneshr@users.noreply.github.com>
2024-02-27 09:06:27 +0530

    Add files via upload

---
+++ OddNumber.java
@@ -0,0 +1,18 @@
+package program;
+
+public class OddNumber {
+
+	public static void main(String[] args) {
+		int a=100;
+		System.out.println("Odd Number :");
+		for(int i=1; i<=a;i++)
+		{
+			if(i%2 !=0)
+			{
+				System.out.print(i+"");			
+				}
+		}
+
+	}
+
+}
