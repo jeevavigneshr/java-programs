2d66d630e7f75bf2f2cc2f2d9d09e0818fa41821
jeevavigneshr <143730328+jeevavigneshr@users.noreply.github.com>
2024-02-28 16:44:33 +0530

    Add files via upload

---
+++ swapping.java
@@ -0,0 +1,20 @@
+package finding;
+
+public class swapping {
+
+	public static void main(String[] args) {
+		int a= 5;
+		int b= 8;
+		int temp = a;
+		a=b;
+		b=temp;
+		System.out.println("a is "+a);
+		System.out.println("b is "+b);
+		
+		 
+		
+		
+
+	}
+
+}
