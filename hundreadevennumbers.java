3b6a25e3e1d406ee0ccf151dd3856041a5673699
jeevavigneshr <143730328+jeevavigneshr@users.noreply.github.com>
2024-03-16 18:42:39 +0530

    Add files via upload

---
+++ hundreadevennumbers.java
@@ -0,0 +1,15 @@
+package finding;
+
+public class hundreadevennumbers {
+
+	public static void main(String[] args) {
+		int even=100;
+		for(int i=1;i<=even;i++) {
+			if(i%2==0) {
+				System.out.println(i);
+			}
+		}
+
+	}
+
+}
