af7a5b83970e4e1646ad46a936a2638a5b677864
jeevavigneshr <143730328+jeevavigneshr@users.noreply.github.com>
2024-03-16 18:37:28 +0530

    Add files via upload

---
+++ hundreadoddnumbers.java
@@ -0,0 +1,11 @@
+package finding;
+
+public class hundreadoddnumbers {
+public static void main(String[]args) {
+int odd =100;
+for(int i=1;i<=odd;i++) {
+	if(i%2!=0) {
+	System.out.println(i);
+}
+}
+}}
