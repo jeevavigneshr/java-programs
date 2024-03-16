38cf5a6949f00d427c49e5175afcd26d42fbb091
jeevavigneshr <143730328+jeevavigneshr@users.noreply.github.com>
2024-03-16 20:39:05 +0530

    Add files via upload

---
+++ patternprogram.java
@@ -0,0 +1,17 @@
+package finding;
+
+public class patternprogram {
+
+	public static void main(String[] args) {
+		int rows=5;
+		for(int i = 1;i<=5;i++) 
+		{
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print("*");
+			}
+System.out.println();
+	}
+
+}
+}
