a49017c245f8e082aac40d2078c3b438c6e7267d
jeevavigneshr <143730328+jeevavigneshr@users.noreply.github.com>
2024-06-29 20:17:54 +0530

    Add files via upload

---
+++ examplewhileloop.java
@@ -0,0 +1,14 @@
+package finding;
+
+public class examplewhileloop {
+
+	public static void main(String[] args) {
+		int number = 3;
+		while(number > 0) {
+			System.out.println("prophet");
+			number--;
+		}
+
+	}
+
+}
