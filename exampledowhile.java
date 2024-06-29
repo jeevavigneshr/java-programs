1dbd9d20f8b5f0a5952fc7c7398011ae14613a32
jeevavigneshr <143730328+jeevavigneshr@users.noreply.github.com>
2024-06-29 20:28:55 +0530

    Add files via upload

---
+++ exampledowhile.java
@@ -0,0 +1,14 @@
+package finding;
+
+public class exampledowhile {
+
+	public static void main(String[] args) {
+		int number=3;
+		do {
+			System.out.println("invades");
+			number--;
+		}while(number > 0);
+
+	}
+
+}
